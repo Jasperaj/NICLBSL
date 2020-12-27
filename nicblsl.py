566b1b49820dc1cb756b784f43cf4f3e12d9440f
Jasperaj <46399293+Jasperaj@users.noreply.github.com>
2020-12-27 09:31:41 +0545

    Add files via upload

---
+++ nicblsl.py
@@ -0,0 +1,13 @@
+import bs4
+import requests
+from bs4 import BeautifulSoup
+import time
+
+
+re = requests.get('http://nepalstock.com.np/marketdepthofcompany/2887')
+soup = bs4.BeautifulSoup(re.text, "lxml")
+Position=soup.find('label',{"class":"livePrice"}).text.strip()
+print(Position)
+
+if Position == '759.00':
+    print("not changed")
